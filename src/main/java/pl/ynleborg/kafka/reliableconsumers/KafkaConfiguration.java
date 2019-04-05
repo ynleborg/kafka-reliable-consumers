fb0b6b1bf6129ccc222f7622ab3c8b76b34fa499
Jacek Grobelny <jgrobelny@consdata.pl>
2019-04-05 19:32:57 +0200

    configuration externalized

--- src/main/java/pl/ynleborg/kafka/reliableconsumers/KafkaConfiguration.java
+++ src/main/java/pl/ynleborg/kafka/reliableconsumers/KafkaConfiguration.java
@@ -1,26 +1,33 @@
 package pl.ynleborg.kafka.reliableconsumers;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @EnableKafka
 @Configuration
+@RequiredArgsConstructor
 public class KafkaConfiguration {
+
+    @Value("${server}")
+    private String server;
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.107:32768");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "reliableconsumers");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "reliableconsumers_string");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config);
@@ -30,6 +37,25 @@ public class KafkaConfiguration {
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
         factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Message> messageConsumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "reliableconsumers_json");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+                new JsonDeserializer<>(Message.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Message> messageKafkaListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Message> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(messageConsumerFactory());
         return factory;
     }
 
--- src/main/java/pl/ynleborg/kafka/reliableconsumers/KafkaConsumer.java
+++ src/main/java/pl/ynleborg/kafka/reliableconsumers/KafkaConsumer.java
@@ -1,13 +1,19 @@
 package pl.ynleborg.kafka.reliableconsumers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "${topic.main}")
-    public void consume(String message) {
-        System.out.println("Consumed message: " + message);
+    @KafkaListener(topics = "${topic.main}",
+            containerFactory = "messageKafkaListenerFactory")
+    public void consumeJson(Message message) {
+        log.info("Consumed Message = {}", message);
+        if (message.getAction() != null && message.getAction().startsWith("retry")) {
+            log.warn("Message is broken, sending to retry topic");
+        }
     }
 }
